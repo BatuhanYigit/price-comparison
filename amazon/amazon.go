623e5b91b51cc39b8505d26de5b1ac52a83f089d
BatuhanYigit <batuhanyigit34@gmail.com>
2023-12-06 11:06:16 +0300

    Add Trendyol price check
    Add List All Product
    Add Trendyol price

--- amazon/amazon.go
+++ amazon/amazon.go
@@ -9,6 +9,14 @@ import (
 	"github.com/gocolly/colly"
 )
 
+type Product struct {
+	Source    string `json:"source"`
+	Link      string `json:"link"`
+	Name      string `json:"name"`
+	Price     string `json:"price"`
+	ImageLink string `json:"imagelink"`
+}
+
 type Item struct {
 	Link      string `json:"link"`
 	Name      string `json:"name"`
@@ -33,11 +41,15 @@ type Trendyol struct {
 func AmazonProduct() {
 	c := colly.NewCollector()
 
-	items := []Item{}
+	products := []Product{}
 
-	amazons := []Amazon{}
+	// items := []Item{}
 
-	trendyols := []Trendyol{}
+	// amazons := []Amazon{}
+
+	// trendyols := []Trendyol{}
+
+	//-----------------------------------------Hepsiburada--------------------------------------------\\
 
 	c.OnRequest(func(r *colly.Request) {
 		r.Headers.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
@@ -46,13 +58,14 @@ func AmazonProduct() {
 	c.OnHTML("section.detail-main ", func(h *colly.HTMLElement) {
 		split := strings.Split(h.ChildText(".price"), "\n")
 		price := strings.TrimSpace(split[0])
-		i := Item{
+		i := Product{
+			Source:    "Hepsiburada",
 			Link:      h.ChildAttr("a", "href"),
 			Name:      h.ChildText("h1"),
 			Price:     price,
 			ImageLink: h.ChildAttr("img", "src"),
 		}
-		items = append(items, i)
+		products = append(products, i)
 	})
 
 	err := c.Visit("https://www.hepsiburada.com/steelseries-arctis-1-wireless-oyuncu-kulakligi-usb-c-wireless-ps4-pc-nintendo-switch-android-uyumlu-pm-HB00000NAMDL")
@@ -60,6 +73,8 @@ func AmazonProduct() {
 		log.Fatal(err)
 	}
 
+	//-----------------------------------------Amazon--------------------------------------------\\
+
 	c.OnRequest(func(r *colly.Request) {
 		r.Headers.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
 	})
@@ -67,13 +82,14 @@ func AmazonProduct() {
 	c.OnHTML("div.a-container ", func(h *colly.HTMLElement) {
 		split := strings.Split(h.ChildText(".a-section"), "TL")
 		price := strings.TrimSpace(split[0])
-		i := Amazon{
+		i := Product{
+			Source:    "Amazon",
 			Link:      h.ChildAttr("a", "href"),
 			Name:      h.ChildText("h1"),
 			Price:     price,
 			ImageLink: h.ChildAttr("img", "src"),
 		}
-		amazons = append(amazons, i)
+		products = append(products, i)
 	})
 
 	err = c.Visit("https://www.amazon.com.tr/SteelSeries-Arctis-Wireless-Gaming-Kulakl%C4%B1k/dp/B09GW7NHL4/ref=sr_1_1?crid=13YG3B9ISWB9N&keywords=steelseries+arctis+7&qid=1701335813&sprefix=steel+%2Caps%2C161&sr=8-1")
@@ -81,20 +97,23 @@ func AmazonProduct() {
 		log.Fatal(err)
 	}
 
+	//-----------------------------------------Trendyol--------------------------------------------\\
+
 	c.OnRequest(func(r *colly.Request) {
 		r.Headers.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
 	})
 
-	c.OnHTML("div.a-container ", func(h *colly.HTMLElement) {
-		split := strings.Split(h.ChildText(".a-section"), "TL")
+	c.OnHTML("div.product-detail-container", func(h *colly.HTMLElement) {
+		split := strings.Split(h.ChildText("span.prc-dsc"), "TL")
 		price := strings.TrimSpace(split[0])
-		i := Trendyol{
+		i := Product{
+			Source:    "Trendyol",
 			Link:      h.ChildAttr("a", "href"),
 			Name:      h.ChildText("h1"),
 			Price:     price,
 			ImageLink: h.ChildAttr("img", "src"),
 		}
-		trendyols = append(trendyols, i)
+		products = append(products, i)
 	})
 
 	err = c.Visit("https://www.trendyol.com/steelseries/arctis-1-wireless-oyuncu-kulaklik-pc-ps4-ps5-xbox-nintendo-switch-ve-android-p-31622422?boutiqueId=61&merchantId=220053")
@@ -102,16 +121,25 @@ func AmazonProduct() {
 		log.Fatal(err)
 	}
 
-	data, err := json.MarshalIndent(items, " ", "")
+	// data, err := json.MarshalIndent(items, " ", "")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println("HepsiBurada Product")
+	// fmt.Println(string(data))
+
+	// data2, err := json.MarshalIndent(amazons, " ", "")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println("Amazon Product")
+	// fmt.Println(string(data2))
+
+	data, err := json.MarshalIndent(products, " ", "")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("HepsiBurada Product")
+	fmt.Println("All Products")
 	fmt.Println(string(data))
-	data2, err := json.MarshalIndent(amazons, " ", "")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Amazon Product")
-	fmt.Println(string(data2))
+
 }
