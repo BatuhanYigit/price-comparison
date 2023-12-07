a8a54060655a7c2282c2c75b5605b811138ea856
BatuhanYigit <batuhanyigit34@gmail.com>
2023-12-07 11:48:16 +0300

    Add GetBy Source
    Add GetBy Source

--- helper/excell.go
+++ helper/excell.go
@@ -2,6 +2,7 @@ package helper
 
 import (
 	"encoding/csv"
+	"fmt"
 	"log"
 	"os"
 )
@@ -46,4 +47,82 @@ func ExcelWriter(products []Product) {
 	}
 
 	defer writer.Flush()
+}
+
+func GetAll() {
+	file, err := os.Open("./products.csv")
+	if err != nil {
+		fmt.Println("File open error", err)
+		return
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+
+	lines, err := reader.ReadAll()
+
+	if err != nil {
+		fmt.Println("File read error ", err)
+		return
+	}
+
+	for _, line := range lines {
+		fmt.Println(line)
+	}
+
+}
+
+func GetBySource(products []Product, source string) []Product {
+	var filterProducts []Product
+
+	for _, product := range products {
+		if product.Source == source {
+			filterProducts = append(filterProducts, product)
+		}
+	}
+
+	return filterProducts
+}
+
+func ReadCSV() ([]Product, error) {
+	filePath := "./products.csv"
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+
+	var products []Product
+	headers, err := reader.Read()
+	if err != nil {
+		return nil, err
+	}
+
+	for {
+		line, err := reader.Read()
+		if err != nil {
+			break
+		}
+
+		product := Product{}
+		for i, value := range line {
+			switch headers[i] {
+			case "Source":
+				product.Source = value
+			case "Link":
+				product.Link = value
+			case "Name":
+				product.Name = value
+			case "Price":
+				product.Price = value
+			case "ImageLink":
+				product.ImageLink = value
+			}
+		}
+		products = append(products, product)
+	}
+
+	return products, nil
 }
